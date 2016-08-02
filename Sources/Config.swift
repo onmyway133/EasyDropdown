25572fb103ec048c5c351fd1ba0d3c40c2a95ca0
Khoa Pham <onmyway133@gmail.com>
2016-08-02 21:23:53 +0200

    Setup TableCell with auto layout

--- Sources/Config.swift
+++ Sources/Config.swift
@@ -12,16 +12,23 @@ public struct Config {
 
   public struct List {
 
-    public struct Text {
-      public static var color: UIColor = UIColor.whiteColor()
-      public static var font: UIFont = UIFont.boldSystemFontOfSize(15)
+    public struct DefaultCell {
+      
+      public struct Text {
+        public static var color: UIColor = UIColor.blackColor()
+        public static var font: UIFont = UIFont.boldSystemFontOfSize(15)
+      }
+
+      public static var backgroundColor: UIColor = UIColor.whiteColor()
     }
 
     public struct Cell {
       public static var type: UITableViewCell.Type = TableCell.self
       public static var config: (cell: UITableViewCell, item: String, index: Int, selected: Bool) -> Void = { cell, item, index, selected in
-        cell.textLabel?.text = item
-        cell.accessoryType = selected ? .Checkmark : .None
+        guard let cell = cell as? TableCell else { return }
+
+        cell.label.text = item
+        cell.checkmark.hidden = !selected
       }
     }
 
--- Sources/TableCell.swift
+++ Sources/TableCell.swift
@@ -2,4 +2,64 @@ import UIKit
 
 public class TableCell: UITableViewCell {
 
+  public lazy var label: UILabel = self.makeLabel()
+  public lazy var checkmark: UIImageView = self.makeCheckmark()
+
+  // MARK: - Initialization
+
+  public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+    contentView.addSubview(label)
+    contentView.addSubview(checkmark)
+
+    setupContraints()
+
+    backgroundColor = Config.List.DefaultCell.backgroundColor
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+
+    contentView.frame = bounds
+  }
+
+  // MARK: - Controls
+
+  func makeLabel() -> UILabel {
+    let label = UILabel()
+    label.textColor = Config.List.DefaultCell.Text.color
+    label.font = Config.List.DefaultCell.Text.font
+
+    return label
+  }
+
+  func makeCheckmark() -> UIImageView {
+    let checkmark = UIImageView(image: AssetManager.image("checkmark")?.imageWithRenderingMode(.AlwaysTemplate))
+    checkmark.tintColor = Config.List.DefaultCell.Text.color
+
+    return checkmark
+  }
+
+  // MARK: - Constraints
+
+  func setupContraints() {
+    // Label
+    label.translatesAutoresizingMaskIntoConstraints = false
+
+    contentView.addConstraint(NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
+    contentView.addConstraint(NSLayoutConstraint(item: label, attribute: .Left, relatedBy: .Equal, toItem: contentView, attribute: .Left, multiplier: 1, constant: 10))
+
+    // Checkmark
+    checkmark.translatesAutoresizingMaskIntoConstraints = false
+    
+    contentView.addConstraint(NSLayoutConstraint(item: checkmark, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
+    contentView.addConstraint(NSLayoutConstraint(item: checkmark, attribute: .Right, relatedBy: .Equal, toItem: contentView, attribute: .Right, multiplier: 1, constant: -10))
+    contentView.addConstraint(NSLayoutConstraint(item: checkmark, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 15))
+    contentView.addConstraint(NSLayoutConstraint(item: checkmark, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 15))
+  }
 }
