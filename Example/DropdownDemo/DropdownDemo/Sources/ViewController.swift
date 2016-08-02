b5a1d6f34dfbebca3a648bf3523ad53000bd955a
Khoa Pham <onmyway133@gmail.com>
2016-08-02 20:47:05 +0200

    Set translucent to false

--- Example/DropdownDemo/DropdownDemo/Sources/ViewController.swift
+++ Example/DropdownDemo/DropdownDemo/Sources/ViewController.swift
@@ -3,11 +3,14 @@ import Dropdown
 
 class ViewController: UIViewController {
 
+  let color = UIColor(red: 22/255, green: 160/255, blue: 33/255, alpha: 1)
+
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = UIColor.whiteColor()
 
-    navigationController?.navigationBar.barTintColor = UIColor(red: 22/255, green: 160/255, blue: 33/255, alpha: 1)
+    navigationController?.navigationBar.translucent = false
+    navigationController?.navigationBar.barTintColor = color
 
     setupNavigationItem()
   }
@@ -15,6 +18,7 @@ class ViewController: UIViewController {
   // MARK: - Navigation
 
   func setupNavigationItem() {
+
     let items = ["World", "Sports", "Culture", "Business", "Travel"]
     let titleView = TitleView(navigationController: navigationController!, title: "Menu", items: items)
     titleView?.action = { index in
